d4417eaf58031547dbb010233cc8569331442e4e
Jonathan Parkinson <jonathan.parkinson@protillion.com>
2026-05-19 12:49:42 -0700

    Minor updates to init sm eval script.

--- run_init_sm_eval.py
+++ run_init_sm_eval.py
@@ -6,8 +6,9 @@ import subprocess
 
 def run_init_sm_eval(input_dir, output_dir, pmpnn_dir):
     os.chdir(input_dir)
-    input_filelist = [os.path.abspath(f) for f in os.listdir()]
-    os.chdir(pmpnn_dir)
+    input_filelist = [os.path.abspath(f) for f in os.listdir()
+                      if "model_0." in f]
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     for input_file in input_filelist:
         try:
