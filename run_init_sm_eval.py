d7745557d0047e42164b71324df999acecb67be8
Jonathan Parkinson <jonathan.parkinson@protillion.com>
2026-05-19 13:35:21 -0700

    Minor updates to init sm eval script.

--- run_init_sm_eval.py
+++ run_init_sm_eval.py
@@ -9,10 +9,11 @@ def run_init_sm_eval(input_dir, output_dir):
     input_filelist = [os.path.abspath(f) for f in os.listdir()
                       if "model_0." in f]
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir("examples")
 
     for input_file in input_filelist:
         try:
-            _=subprocess.run(["./examples/smorodina_init_score.sh",
+            _=subprocess.run(["./smorodina_init_score.sh",
                 input_file, output_dir], check=True
             )
         except Exception as E:
